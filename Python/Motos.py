fcd921bd6305a47947a69738ba020b628d08f994
Raúl <raulsarmientoperez@gmail.com>
2025-02-07 12:06:41 +0000

    Update Motos.py

--- Python/Motos.py
+++ Python/Motos.py
@@ -1,7 +1,7 @@
 #Pedimos los datos
 vel1 = float(input("¿Qué velocidad tiene la primera moto? (km/h)"))
 vel2 = float(input("¿Qué velocidad tiene la segunda moto? (km/h)"))
-dis = float(input("¿A qué distancia se encuentra una de la otra? (h)"))
+dis = float(input("¿A qué distancia se encuentra una de la otra? (km)"))
 #Definimos la función
 def calculo(vel1,vel2,dis):
  if vel1 >= vel2: #En caso de que la moto 1 vaya más rápido que la moto 2
